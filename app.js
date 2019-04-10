a310e356599e3d3157dd1b6686d514183932506c
Eugeny Klokov <grafinfector@hotmail.com>
2019-04-10 17:55:13 +0300

    requirejs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = {}; // application
 require.config({
     baseUrl: 'engine',
     paths: {
-       jquery: 'js/jquery',
+       jquery: 'js/jquery-3.3.1.min',
        message: 'js/message'
     }
  });
@@ -16,5 +16,12 @@ require(['jquery'], function ($) {
 var initialize = function() {
     var canvas = document.getElementById("rMind.Canvas");
     ctx = canvas.getContext('2d');
-    ctx.fillRect(20, 20, 200, 140);   
+    
+    ctx.shadowBlur = 10;
+    ctx.shadowColor = "black";
+    ctx.shadowOffsetY = 10;
+
+    ctx.fillRect(50, 50, 200, 140); 
+    ctx.strokeStyle = "#FF0000";
+    ctx.strokeRect(50, 50, 200, 140);       
 }
